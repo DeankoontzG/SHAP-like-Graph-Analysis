70765f907392c49979ab63d959b1e06b47d4b37a
Guilhem Dupuy <guilhempds@gmail.com>
2026-02-27 09:00:57 +0000

    ADD - ouptus w ratio links_nonlinks de 1 pour 10

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
+import json
+import numpy as np
 import networkx as nx
-import html
-import io
-from src.SHAP_like_graph_tool import user_usable_functions as kk
+import pandas as pd
+from src.SHAP_like_graph_tool import user_usable_functions as gp
 from src.SHAP_like_graph_tool import utils 
 
 
 if __name__ == "__main__":
 
-    def load_graphml_safe(path):
-        with open(path, 'r', encoding='utf-8') as f:
-            raw_data = f.read()
-
-        clean_data = html.unescape(raw_data)
-        G = nx.read_graphml(io.StringIO(clean_data))
-        
-        print(f"✅ Graphe chargé : {G.number_of_nodes()} nœuds et {G.number_of_edges()} liens.")
-        return G
-
-    G = load_graphml_safe("Airports.graphml")
-    print("--- Démarrage du Link Prediction Tool ---")
+    for i in np.arange(1, 1.05, 0.05):
     
-    ## Test de la fonction principale
-    shap_exp = utils.load_shap_analysis()
-
-    features = shap_exp.values[1]
-    print(features)
+        print("######################################")
+        print(f"#### graph sbm {i:.2f} pos {1-i:.2f} ####")
+        print("######################################")
+        G_name = f"artificial_graph_sbm_{f'{i:.2f}'.replace('.', '_')}_pos_{f'{1-i:.2f}'.replace('.', '_')}"
+        print(G_name)
+    
+        with open(f"graph_library/{G_name}.json", 'r', encoding='utf-8') as f:
+            data = json.load(f)  
+    
+        try:
+            G = nx.node_link_graph(data)
+            print(f"Graphe chargé avec succès : {G.number_of_nodes()} nœuds et {G.number_of_edges()} liens.")
+        except Exception as e:
+            print(f"Erreur lors de la conversion : {e}")
+                
+        gp.execute(G,G_name)
