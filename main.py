07107c137c0889c78fdf213555d665d26f5fcdf7
Guilhem Dupuy <guilhempds@gmail.com>
2026-03-13 10:23:26 +0000

    UPDATE - new graphs reels et code modifs

--- main.py
+++ main.py
@@ -36,20 +36,55 @@ from sklearn.model_selection import KFold, train_test_split
 from xgboost import XGBClassifier
 import optuna
 
+# nohup python -u main.py 2>&1 | grep --line-buffered -vE "it/s|%|\[.*\]" | grep --line-buffered "." > myoutfile.log &
+
+def load_graphml_safe(path):
+        with open(path, 'r', encoding='utf-8') as f:
+            raw_data = f.read()
+
+        clean_data = html.unescape(raw_data)
+        G = nx.read_graphml(io.StringIO(clean_data))
+        
+        print(f"✅ Graphe chargé : {G.number_of_nodes()} nœuds et {G.number_of_edges()} liens.")
+        return G
+
 if __name__ == "__main__":
+
+    names_list = [#"blumenau_drug",
+                  #"facebook_friends",
+                  "cintestinalis",
+                  "faculty_hiring_computer_science", 
+                  "jazz_collab",
+                  "wiki_science",
+                  "Airports"]
+                  
     
-    for i in np.arange(0.00, 1.25, 0.25):
+    for name in names_list:
         print("######################################")
-        print(f"#### graph sbm v2 {i:.2f} pos {1-i:.2f} ####")
+        print(f"#### graph {name} :  ####")
         print("######################################")
-        G_name = f"artificial_graph_sbmv2_{f'{i:.2f}'.replace('.', '_')}_pos_{f'{1-i:.2f}'.replace('.', '_')}"
+        G_name = f"reel_{name}"  
+        path = f"graph_library/{G_name}.graphml"
         print(G_name)
     
-        with open(f"graph_library/{G_name}.json", 'r', encoding='utf-8') as f:
-            data = json.load(f)  
+        try:
+            G = load_graphml_safe(path)
+            print(f"Graphe chargé avec succès : {G.number_of_nodes()} nœuds et {G.number_of_edges()} liens.")
+        except Exception as e:
+            print(f"Erreur lors de la conversion : {e}")
+    
+        gp.execute(G, G_name)
+
+    for name in names_list:
+        print("######################################")
+        print(f"#### graph {name} :  ####")
+        print("######################################")
+        G_name = f"reel_{name}"  
+        path = f"graph_library/{G_name}.graphml"
+        print(G_name)
     
         try:
-            G = nx.node_link_graph(data)
+            G = load_graphml_safe(path)
             print(f"Graphe chargé avec succès : {G.number_of_nodes()} nœuds et {G.number_of_edges()} liens.")
         except Exception as e:
             print(f"Erreur lors de la conversion : {e}")
